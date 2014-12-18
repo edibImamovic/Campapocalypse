0bdbeefa8f52deba9e7acb1d165852da87a5e8c1
edibi <edibimamovic@10.0.82.13>
2014-12-18 11:31:33 +0100

    Zombie dodao

---
+++ src/Zombie.java
@@ -0,0 +1,9 @@
+
+public class  Zombie extends Character {
+
+	public Zombie(String name, double healthP, double dex, double str, double spd, Weapon wpn) {
+		super(name, healthP, dex, str, spd, wpn);
+	}
+	
+	
+}
