3b6ba65d1cdc07774935ed6e38b970be04148c42
Mustafa Ademovic <mustafa.ademovic93@gmail.com>
2014-12-18 10:58:56 +0100

    Class Player
    Uradio Mustafa

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,23 @@
 
-public class Player {
+public class Player extends Character {
+	private double intl;
+
+	public Player(String name, double healthP, double dex, double str, double spd, Weapon wpn, double intl) {
+		super(name, healthP, dex, str, spd, wpn);
+		this.intl = intl;
+
+	}
+
+	public double getIntl() {
+		return intl;
+	}
+
+	public void setIntl(double intl) {
+		this.intl = intl;
+	}
+
+	public String toString() {
+		return super.toString() + "\nIntelegent: " + intl;
+	}
 
 }
