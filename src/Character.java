45eaf13e0e57c5f723fae5f1bae9733eda3e164e
Nermin.Graca <nera.graca@gmail.com>
2014-12-18 10:12:59 +0100

    Project Campocalypse
    Radili Edib, Mustafa, Neldin i Nermin

---
+++ src/Character.java
@@ -0,0 +1,90 @@
+
+public class Character {
+	
+	String name;
+	double healthP;
+	double dex;
+	double str;
+	double spd;
+	Weapon wpn;
+	
+
+	public Character(String name, double healthP, double dex, double str, double spd, Weapon wpn) {
+		this.name = name;
+		this.healthP = healthP;
+		this.dex = dex;
+		this.str = str;
+		this.spd = spd;
+		this.wpn = wpn;
+	}
+
+	public String toString() {
+		return "Character [name=" + name + ", \nhealthP=" + healthP + ", \ndex="
+				+ dex + ", \nstr=" + str + ", \nspd=" + spd + "]\n";
+	}
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getHealthP() {
+		return healthP;
+	}
+
+
+	public void setHealthP(double healthP) {
+		this.healthP = healthP;
+	}
+
+
+	public double getDex() {
+		return dex;
+	}
+
+
+	public void setDex(double dex) {
+		this.dex = dex;
+	}
+
+
+	public double getStr() {
+		return str;
+	}
+
+
+	public void setStr(double str) {
+		this.str = str;
+	}
+
+
+	public double getSpd() {
+		return spd;
+	}
+
+
+	public void setSpd(double spd) {
+		this.spd = spd;
+	}
+
+
+	public Weapon getWpn() {
+		return wpn;
+	}
+
+
+	public void setWpn(Weapon wpn) {
+		this.wpn = wpn;
+	}
+	
+	
+
+}
